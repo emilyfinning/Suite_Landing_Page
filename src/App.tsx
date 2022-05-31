a1f10e87b74d69bd320e3e24b2917a82d86dc99d
emilyfinning <em@emilyjay.co.uk>
2022-05-31 14:50:24 +0100

    Set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="bg-pale">Hello</div>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "dark-blue": "#172339",
+      "grey-blue": "#49566D",
+      beige: "#F3EDE7",
+      pale: "#FAF8F6",
+      purple: "#A060FF",
+      pink: "#CB30E3",
+      orange: "#FFA84E",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
